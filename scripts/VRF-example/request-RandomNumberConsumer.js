e5454f92fda6d3352b376c59515e12f376a5a4d8
masaun <masanoriuno@gmail.com>
2021-11-13 20:53:25 +0900

    [Script]: Re-write scripts/request-RandomNumberConsumer.js from Truffle to Hardhat

--- scripts/VRF-example/request-RandomNumberConsumer.js
+++ scripts/VRF-example/request-RandomNumberConsumer.js
@@ -1,11 +1,23 @@
-const RandomNumberConsumer = artifacts.require('RandomNumberConsumer')
+const { ethers } = require("hardhat")
 
-module.exports = async (deployer, network, [defaultAccount]) => {
-    // Local (development) networks need their own deployment of the LINK
-    // token and the Oracle contract
-    if (!network.startsWith('rinkeby')) {
-        console.log("We can deploy stuff... but that's it!")
-    }
-    const randomNumberConsumer = await RandomNumberConsumer.deployed()
-    //console.log(await randomNumberConsumer.weather())
+
+async function main() {
+    // [NOTE]: Deployed-address of the RandomNumberConsumer.sol on Rinkeby is "0x461f9bD7A33B9532a19BC7B7834063c313058e32"
+
+    //@dev - Get the contract to deploy
+    const RANDOM_NUMBER_CONSUMER = "0x461f9bD7A33B9532a19BC7B7834063c313058e32"
+    //const RandomNumberConsumer = await ethers.getContractFactory("RandomNumberConsumer")
+    //const randomNumberConsumer = await RandomNumberConsumer.deploy()
+    const randomNumberConsumer = await ethers.getContractAt("RandomNumberConsumer", RANDOM_NUMBER_CONSUMER)
+    console.log("Deployed-address of the RandomNumberConsumer.sol on Rinkeby", randomNumberConsumer.address)  // [Result]: "0x461f9bD7A33B9532a19BC7B7834063c313058e32"
 }
+
+/**
+ * @dev - Execute scripts
+ */ 
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
