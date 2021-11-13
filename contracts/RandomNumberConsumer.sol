9edf9eb868455f46098d6a9a1d0fc3e307cd2098
masaun <masanoriuno@gmail.com>
2021-11-13 20:20:00 +0900

    [Deploy]: the RandomNumberConsumer.sol by using Hardhat

--- contracts/RandomNumberConsumer.sol
+++ contracts/RandomNumberConsumer.sol
@@ -17,10 +17,11 @@ contract RandomNumberConsumer is VRFConsumerBase {
     /**
      * Constructor inherits VRFConsumerBase
      *
-     * Network: Kovan
+     * Network: Rinkeby
      * Chainlink VRF Coordinator address: 0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9
-     * LINK token address:                0xa36085F69e2889c224210F603D836748e7dC0088
-     * Key Hash: 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4
+     * LINK token address:                0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B
+     * Key Hash: 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311
+     * Fee: 0.1 LINK
      */
     constructor(address _vrfCoordinator,
                 address _link,
