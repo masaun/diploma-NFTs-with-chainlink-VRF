7f6fa1a1ba54d783165bd542e7adc70bb6c78c2d
masaun <masanoriuno@gmail.com>
2021-11-14 10:59:25 +0900

    [SC]: Add LINK.transferFrom() to inside of getRandomNumber()

--- scripts/VRF-example/request-RandomNumberConsumer.js
+++ scripts/VRF-example/request-RandomNumberConsumer.js
@@ -2,6 +2,11 @@ const { ethers } = require("hardhat")
 
 
 async function main() {
+    //@dev - Create the LINK token contract interface on Rinkeby    
+    const LINK_TOKEN = "0x01be23585060835e02b77ef475b0cc51aa1e0709"
+    const linkToken = await ethers.getContractAt('@chainlink/contracts/src/v0.6/interfaces/LinkTokenInterface.sol:LinkTokenInterface', LINK_TOKEN)
+
+
     // [NOTE]: Deployed-address of the RandomNumberConsumer.sol on Rinkeby is "0x461f9bD7A33B9532a19BC7B7834063c313058e32"
 
     //@dev - Get the contract to deploy
@@ -13,12 +18,15 @@ async function main() {
 
     //@dev - Test getRandomNumber()
     console.log('Should successfully make an external random number request')
+    const linkAmount = ethers.utils.parseEther('1')  // 1 LINK
+    const txReceipt1 = await linkToken.approve(RANDOM_NUMBER_CONSUMER, linkAmount)
     const transaction = await randomNumberConsumer.getRandomNumber()
-    console.log(`\n transaction: ${ JSON.stringify(transaction, null, 2) }`)  /// [NOTE]: Using "JSON.stringify()" to avoid that value is "[object object]"
+    //console.log(`\n transaction: ${ JSON.stringify(transaction, null, 2) }`)  /// [NOTE]: Using "JSON.stringify()" to avoid that value is "[object object]"
 
     const tx_receipt = await transaction.wait(1)
-    console.log(`\n tx_receipt: ${ JSON.stringify(tx_receipt, null, 2) }`)    /// [NOTE]: Using "JSON.stringify()" to avoid that value is "[object object]"
-    const requestId = await JSON.stringify(tx_receipt).events
+    //console.log(`\n tx_receipt: ${ JSON.stringify(tx_receipt, null, 2) }`)    /// [NOTE]: Using "JSON.stringify()" to avoid that value is "[object object]"
+    const requestId = tx_receipt.events[2].topics[1]
+    //const requestId = await JSON.stringify(tx_receipt).events
     //const requestId = JSON.stringify(tx_receipt).events[2].topics[1]
     console.log("requestId: ", requestId)
 
