ae8565012721ad227ac44ab92951c9a31534959d
masaun <masanoriuno@gmail.com>
2021-11-17 19:07:53 +0900

    [Script]: on Rinkeby -> It takes longer time than Kovan

--- scripts/VRF-example/request-RandomNumberConsumer.js
+++ scripts/VRF-example/request-RandomNumberConsumer.js
@@ -5,24 +5,20 @@ async function main() {
 
     console.log('Should successfully make an external random number request')
 
-    //@dev - Create the LINK token contract interface on Kovan
-    const LINK_TOKEN = "0xa36085F69e2889c224210F603D836748e7dC0088"
+    //@dev - Create the LINK token contract interface on Kovan, Rinkeby
+    //const LINK_TOKEN = "0xa36085F69e2889c224210F603D836748e7dC0088"  // Kovan
+    const LINK_TOKEN = "0x01BE23585060835E02B77ef475b0Cc51aA1e0709"    // Rinkeby
     const linkToken = await ethers.getContractAt('@chainlink/contracts/src/v0.6/interfaces/LinkTokenInterface.sol:LinkTokenInterface', LINK_TOKEN)
 
     // [NOTE]: Deployed-address of the RandomNumberConsumer.sol on Kovan is "0x5d41a0292A7381321A65d430Dda70a7b433a49B5"
 
     //@dev - Get the contract to deploy
-    const RANDOM_NUMBER_CONSUMER = "0x5d41a0292A7381321A65d430Dda70a7b433a49B5"
+    //const RANDOM_NUMBER_CONSUMER = "0x5d41a0292A7381321A65d430Dda70a7b433a49B5"  // Kovan
+    const RANDOM_NUMBER_CONSUMER = "0xbBe48Fb018BAF729Be2D51387CeB121FF35E24D3"    // Rinkeby
     //const RandomNumberConsumer = await ethers.getContractFactory("RandomNumberConsumer")
     //const randomNumberConsumer = await RandomNumberConsumer.deploy()
     const randomNumberConsumer = await ethers.getContractAt("RandomNumberConsumer", RANDOM_NUMBER_CONSUMER)
     console.log("Deployed-address of the RandomNumberConsumer.sol on Kovan", randomNumberConsumer.address) 
-
-
-    // const provider = new ethers.providers.JsonRpcProvider()
-    // const signer = provider.getSigner()
-    // const randomNumberConsumerWithSigner = randomNumberConsumer.connect(signer)
-    // console.log('randomNumberConsumerWithSigner: ', randomNumberConsumerWithSigner)
 
     //@dev - Test getRandomNumber()
     console.log('Should successfully make an external random number request')
@@ -31,8 +27,12 @@ async function main() {
     const txReceipt1 = await linkToken.approve(RANDOM_NUMBER_CONSUMER, linkAmount) // [Result]: Success
     console.log(`\n txReceipt1 of linkToken.approve(): ${ JSON.stringify(txReceipt1, null, 2) }`)
 
-    const transaction = await randomNumberConsumer.getRandomNumber({ gasLimit: 210640, gasPrice: 100000000000 })  /// [NOTE]: Gas units (limit) * Gas price per unit:  eg). 21,000 * 200 = 4,200,000 gwei or 0.0042 ETH
-    //const transaction = await randomNumberConsumer.getRandomNumber({ gasLimit: 250000, gasPrice: 10000000000000 })  /// [NOTE]: This gasPrice is 10,000,000,000,000 wei (=10,000 Gwei) 
+    /**
+     * [NOTE]: GasFee = GasLimit (Gas Unit) * GasPrice
+     *         eg). 21,000 * 200 = 4,200,000 gwei or 0.0042 ETH
+     */
+    //@dev - Gas Fee the best to call getRandomNumber method = gasLimit (210640 wei) * gasPrice (100000000000 wei = 100 Gwei)
+    const transaction = await randomNumberConsumer.getRandomNumber({ gasLimit: 250000, gasPrice: 1000000000000 })  
     console.log(`\n transaction: ${ JSON.stringify(transaction, null, 2) }`)  /// [NOTE]: Using "JSON.stringify()" to avoid that value is "[object object]"
 
     const tx_receipt = await transaction.wait()
