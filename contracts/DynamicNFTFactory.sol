3030578aa3054dd9b8646a0e57cbd8b15db2e4fb
masaun <masanoriuno@gmail.com>
2021-11-18 09:46:55 +0900

    [SC]:  Define the DynamicNFTFactory.sol

---
+++ contracts/DynamicNFTFactory.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.7;
+
+import { DynamicNFT } from "./DynamicNFT.sol";
+
+/**
+ * @notice - This is a smart contract that manage the DynamicNFT.sol
+ */
+contract DynamicNFTFactory {
+    // [Todo]:
+}
