52564c62168413760bf39cb8a7755c1b5a977eda
masaun <masanoriuno@gmail.com>
2021-11-20 17:35:41 +0900

    [SC]: Define getGraduate() method for calling the Graduate struct that is specified

--- contracts/GraduatesRegistry.sol
+++ contracts/GraduatesRegistry.sol
@@ -1,4 +1,5 @@
 pragma solidity 0.7.6;
+pragma abicoder v2;
 
 import { DiplomaNFT } from "./DiplomaNFT.sol";
 
@@ -8,14 +9,24 @@ import { DiplomaNFT } from "./DiplomaNFT.sol";
 contract GraduatesRegistry {
 
     struct Graduate {
-        bytes32 graduateId;  // [NOTE]: This graduate ID is random number that is retrieved via Chainlink VRF
+        bytes32 graduateId;        // [NOTE]: This graduate ID is random number that is retrieved via Chainlink VRF
+        bytes32 diplomaNFTTokenId; // [NOTE]: Token ID of the DiplomaNFT 
         string graduateName;
         address graduateAddress;
     }
     mapping(bytes32 => Graduate) public graduates;  // [Key]: Gradudate ID -> The Graduate struct
 
+
     constructor() public {
         // [Todo]:
+    }
+
+
+    //----------------
+    // Getter methods
+    //----------------
+    function getGraduate(bytes32 graduateId) public view returns (Graduate memory _graduate) {
+        return graduates[graduateId];
     }
 
 
