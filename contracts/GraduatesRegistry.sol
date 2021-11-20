b85d2a65b6fb05490489e383dce423b9ddf965cc
masaun <masanoriuno@gmail.com>
2021-11-20 17:29:27 +0900

    [SC]: Define the Graduate struct

--- contracts/GraduatesRegistry.sol
+++ contracts/GraduatesRegistry.sol
@@ -6,5 +6,17 @@ import { DiplomaNFT } from "./DiplomaNFT.sol";
  * @notice - This is a smart contract for graduates registry.
  */
 contract GraduatesRegistry {
-    // [Todo]:
+
+    struct Graduate {
+        bytes32 graduateId;  // [NOTE]: This graduate ID is random number that is retrieved via Chainlink VRF
+        string graduateName;
+        address graduateAddress;
+    }
+    mapping(bytes32 => Graduate) public graduates;  // [Key]: Gradudate ID -> The Graduate struct
+
+    constructor() public {
+        // [Todo]:
+    }
+
+
 }
