3fe80a717d51cac19896fb2c806027cb0fe09204
masaun <masanoriuno@gmail.com>
2021-11-20 17:45:21 +0900

    [SC]: Define registerNewGraduate()

--- contracts/GraduatesRegistry.sol
+++ contracts/GraduatesRegistry.sol
@@ -8,6 +8,12 @@ import { DiplomaNFT } from "./DiplomaNFT.sol";
  */
 contract GraduatesRegistry {
 
+    //@dev - Variable to count total number of graduates
+    uint256 graduatesCounter;
+
+    //@dev - Contract instance
+    DiplomaNFT public immutable diplomaNFT;
+
     struct Graduate {
         bytes32 graduateId;        // [NOTE]: This graduate ID is random number that is retrieved via Chainlink VRF
         bytes32 diplomaNFTTokenId; // [NOTE]: Token ID of the DiplomaNFT 
@@ -16,9 +22,34 @@ contract GraduatesRegistry {
     }
     mapping(bytes32 => Graduate) public graduates;  // [Key]: Gradudate ID -> The Graduate struct
 
+    /**
+     * @dev - Constructor 
+     */
+    constructor(DiplomaNFT _diplomaNFT) public {
+        diplomaNFT = _diplomaNFT;
+    }
 
-    constructor() public {
-        // [Todo]:
+    /**
+     * @dev - Register a new graduate
+     */
+    function registerNewGraduate() public returns (bool) {
+        //@dev - Count up total number of graduates
+        graduatesCounter++;
+
+        // [Todo]: Create a new graduates ID that is a requestId which was used for retrieving RN (Random Number) via Chainlink-VRF 
+        bytes32 newGraduateId;
+
+        // [Todo]: Assign values into each properties of the Graduate struct
+        bytes32 newDiplomaNFTTokenId; // [NOTE]: Token ID of the DiplomaNFT 
+        string memory newGraduateName;
+        address newGraduateAddress;
+
+        // [Todo]: Store data of a new graduate
+        Graduate storage graduate = graduates[newGraduateId];
+        graduate.graduateId = newGraduateId;
+        graduate.diplomaNFTTokenId = newDiplomaNFTTokenId;
+        graduate.graduateName = newGraduateName;
+        graduate.graduateAddress = newGraduateAddress;
     }
 
 
