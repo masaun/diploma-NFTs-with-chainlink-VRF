a0262f9440bdd3a21f683080e321c1171243e254
masaun <masanoriuno@gmail.com>
2021-11-21 10:21:55 +0900

    [SC]: Define the DiplomaNFTFactory.sol -> Then, Define createNewDiplomaNFT()

--- contracts/DiplomaNFTFactory.sol
+++ contracts/DiplomaNFTFactory.sol
@@ -6,5 +6,19 @@ import { DiplomaNFT } from "./DiplomaNFT.sol";
  * @notice - This is a smart contract that manage the DiplomaNFT.sol
  */
 contract DiplomaNFTFactory {
-    // [Todo]:
+
+    //@dev - Created-contract address of DiplomaNFTs are assigned into this variable.
+    address[] diplomaNFTsList;
+
+    constructor() public {}
+
+    function createNewDiplomaNFT(address _vrfCoordinator, address _link, bytes32 _keyHash, uint _fee) public returns (DiplomaNFT _newDiplomaNFT) {
+        DiplomaNFT newDiplomaNFT = new DiplomaNFT(_vrfCoordinator, _link, _keyHash, _fee);
+        address NEW_DIPLOMA_NFT = address(newDiplomaNFT);
+
+        diplomaNFTsList.push(NEW_DIPLOMA_NFT);
+
+        return newDiplomaNFT;
+    }
+
 }
